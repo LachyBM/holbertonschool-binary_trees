e12ffa3c39da653693264f3469429168959e460c
LachyBM <13072@holbertonstudents.com>
2026-04-08 15:57:00 +0000

    fix

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -12,8 +12,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (tree->left != NULL && tree->right != NULL && tree->parent != NULL)
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
-	return (0);
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
+
+	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 }
