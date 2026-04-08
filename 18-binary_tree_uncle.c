474cecf00f5c6edc02dc69277fca0850a42d7a43
LachyBM <13072@holbertonstudents.com>
2026-04-08 22:56:46 +0000

    18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * binary_tree_uncle- find node
+ * @node:parent tree
+ * Return: pointer to node or NULL
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
+
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+
+	return (node->parent->parent->left);
+}
