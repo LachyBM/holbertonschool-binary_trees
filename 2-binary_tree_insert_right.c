6ce1bf61a3eda60dafd159524c60e983f0c957d1
LachyBM <13072@holbertonstudents.com>
2026-04-08 02:54:30 +0000

    right add

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_insert_right- add to right node
+ * @parent:parent tree
+ * @value: value of the node
+ * Return: pointer to node or NULL
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (!parent)
+		return (NULL);
+
+	new_node = binary_tree_node(parent, value);
+	if (!new_node)
+		return (NULL);
+
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+	}
+	parent->right = new_node;
+
+	return (new_node);
+}
